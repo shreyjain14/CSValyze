3ed8939bc506517c7d0ba3fcfe9ed37d20987b46
Aleena Varghese <143576861+Aleena24@users.noreply.github.com>
2024-12-06 14:32:35 +0530

    commit

--- src/AlgorithmPage.js
+++ src/AlgorithmPage.js
@@ -113,10 +113,15 @@ const AlgorithmPage = () => {
         </div>
       )}
 
-      {/* Next Button (Simplified) */}
-      <button className="next-button" onClick={handleNextClick}>
-        &rarr;
-      </button>
+      {/* Updated Swipe Button */}
+      <div className="swipe-button-container" onClick={handleNextClick}>
+        <button className="swipe-button">
+          <span>Swipe Next</span>
+          <div className="arrow-container">
+            <div className="arrow"></div>
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
