c5fd5e6d832a10527abed4c772d9c436436ece2d
Aleena Varghese <143576861+Aleena24@users.noreply.github.com>
2024-12-07 14:52:28 +0530

    commit

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -10,43 +10,61 @@ const AboutUs = () => {
     { name: 'Dr. Tejil J John', role: 'Faculty Coordinator', imgSrc: './img/TS.jpeg', linkedIn: 'https://www.linkedin.com/in/dr-tegil-j-john-1b7a34150/' },
 
     // Core Team
-    { name: 'Himanshu Gulecha', role: 'Core Team', imgSrc: './img/HG.png', linkedIn: 'https://www.linkedin.com/in/himanshugulechha?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app' },
-    { name: 'Priyansha Upadayay', role: 'Core Team', imgSrc: './img/PU.png', linkedIn: 'https://www.linkedin.com/in/priyansha1306/' },
-    { name: 'Satyam Kumar', role: 'Core Team', imgSrc: './img/SK.png', linkedIn: 'https://www.linkedin.com/in/satyamkumar09/' },
+    { name: 'Himanshu Gulecha', role: 'Core Team', imgSrc: './img/1.png', linkedIn: 'https://www.linkedin.com/in/himanshugulechha?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app' },
+    { name: 'Priyansha Upadhyay', role: 'Core Team', imgSrc: './img/2.png', linkedIn: 'https://www.linkedin.com/in/priyansha1306/' },
+    { name: 'Satyam Kumar', role: 'Core Team', imgSrc: './img/3.png', linkedIn: 'https://www.linkedin.com/in/satyamkumar09/' },
 
     // Frontend Team
-    { name: 'Steffi Angel C', role: 'Frontend Developer', imgSrc: './img/steffi.jpg', linkedIn: '#' },
-    { name: 'Alex Thompson', role: 'Frontend Developer', imgSrc: './img/alex.jpg', linkedIn: '#' },
-    { name: 'Emily Davis', role: 'Frontend Developer', imgSrc: './img/emily.jpg', linkedIn: '#' },
+    { name: 'Aleena Varghese', role: 'Frontend Developer', imgSrc: './img/4.png', linkedIn: '#' },
+    { name: 'Sanin Zulphi', role: 'Frontend Developer', imgSrc: './img/5.png', linkedIn: '#' },
+    { name: 'Sneha P', role: 'Frontend Developer', imgSrc: './img/6.png', linkedIn: '#' },
 
     // Backend Team
-    { name: 'Daniel Wilson', role: 'Backend Developer', imgSrc: './img/daniel.jpg', linkedIn: '#' },
-    { name: 'Sophia Brown', role: 'Backend Developer', imgSrc: './img/sophia.jpg', linkedIn: '#' },
-    { name: 'Ethan White', role: 'Backend Developer', imgSrc: './img/ethan.jpg', linkedIn: '#' },
+    { name: 'Badrinarayanan S', role: 'Backend Developer', imgSrc: './img/7.png', linkedIn: '#' },
+    { name: 'Kiran Basil Varghese', role: 'Backend Developer', imgSrc: './img/8.png', linkedIn: '#' },
+    { name: 'Kripa Mary Jose', role: 'Backend Developer', imgSrc: './img/9.png', linkedIn: '#' },
 
     // Preprocessing Team
-    { name: 'John Smith', role: 'Preprocessing', imgSrc: './img/john_smith.jpg', linkedIn: '#' },
-    { name: 'Lily Evans', role: 'Preprocessing', imgSrc: './img/lily.jpg', linkedIn: '#' },
-    { name: 'Olivia Martin', role: 'Preprocessing', imgSrc: './img/olivia.jpg', linkedIn: '#' },
-    { name: 'Jacob Green', role: 'Preprocessing', imgSrc: './img/jacob.jpg', linkedIn: '#' },
-    { name: 'Mia Clark', role: 'Preprocessing', imgSrc: './img/mia.jpg', linkedIn: '#' },
-    { name: 'Lucas Carter', role: 'Preprocessing', imgSrc: './img/lucas.jpg', linkedIn: '#' },
-    { name: 'John Smith', role: 'Preprocessing', imgSrc: './img/john_smith.jpg', linkedIn: '#' },
-    { name: 'Lily Evans', role: 'Preprocessing', imgSrc: './img/lily.jpg', linkedIn: '#' },
-    { name: 'Olivia Martin', role: 'Preprocessing', imgSrc: './img/olivia.jpg', linkedIn: '#' },
-    { name: 'Jacob Green', role: 'Preprocessing', imgSrc: './img/jacob.jpg', linkedIn: '#' },
-    { name: 'Mia Clark', role: 'Preprocessing', imgSrc: './img/mia.jpg', linkedIn: '#' },
-    { name: 'Lucas Carter', role: 'Preprocessing', imgSrc: './img/lucas.jpg', linkedIn: '#' },
-    // Add more members as needed for Preprocessing
+    { name: 'Bagyalakshmi S Shinde', role: 'Preprocessing', imgSrc: './img/10.png', linkedIn: '#' },
+    { name: 'C Steffi Angel', role: 'Preprocessing', imgSrc: './img/11A.png', linkedIn: '#' },
+    { name: 'Elizabeth George', role: 'Preprocessing', imgSrc: './img/11.png', linkedIn: '#' },
+    { name: 'Manav Gupta', role: 'Preprocessing', imgSrc: './img/12.png', linkedIn: '#' },
+    { name: 'Nandhitha S', role: 'Preprocessing', imgSrc: './img/13.png', linkedIn: '#' },
+
 
     // Model Implementation Team
-    { name: 'Chris Martin', role: 'Model Implementation', imgSrc: './img/chris.jpg', linkedIn: '#' },
-    { name: 'Ava Taylor', role: 'Model Implementation', imgSrc: './img/ava.jpg', linkedIn: '#' },
-    { name: 'James Wilson', role: 'Model Implementation', imgSrc: './img/james.jpg', linkedIn: '#' },
-    { name: 'Chris Martin', role: 'Model Implementation', imgSrc: './img/chris.jpg', linkedIn: '#' },
-    { name: 'Ava Taylor', role: 'Model Implementation', imgSrc: './img/ava.jpg', linkedIn: '#' },
-    { name: 'James Wilson', role: 'Model Implementation', imgSrc: './img/james.jpg', linkedIn: '#' },
-    // Add more members as needed for Model Implementation
+    { name: 'Christina J Thattil', role: 'Model Implementation', imgSrc: './img/14.png', linkedIn: '#' },
+    { name: 'Deyon Rose Babu Thomas', role: 'Model Implementation', imgSrc: './img/15.png', linkedIn: '#' },
+    { name: 'Dhyan R', role: 'Model Implementation', imgSrc: './img/16.png', linkedIn: '#' },
+    { name: 'Epista R', role: 'Model Implementation', imgSrc: './img/17.png', linkedIn: '#' },
+    { name: 'Ismail Basheer', role: 'Model Implementation', imgSrc: './img/18.png', linkedIn: '#' },
+    { name: 'Karthikeyan K J', role: 'Model Implementation', imgSrc: './img/19.png', linkedIn: '#' },
+    { name: 'M Surendran', role: 'Model Implementation', imgSrc: './img/20.png', linkedIn: '#' },
+    { name: 'Manu Martin', role: 'Model Implementation', imgSrc: './img/22.png', linkedIn: '#' },
+    { name: 'Marcus Daniel P', role: 'Model Implementation', imgSrc: './img/23.png', linkedIn: '#' },
+    { name: 'Melvin Infant A', role: 'Model Implementation', imgSrc: './img/24.png', linkedIn: '#' },
+    { name: 'Muzzammil Razin P A', role: 'Model Implementation', imgSrc: './img/25.png', linkedIn: '#' },
+    { name: 'Neha Seirah Biju', role: 'Model Implementation', imgSrc: './img/26.png', linkedIn: '#' },
+    { name: 'Ponwinsiya P', role: 'Model Implementation', imgSrc: './img/27.png', linkedIn: '#' },
+    { name: 'Pradakshina P', role: 'Model Implementation', imgSrc: './img/28.png', linkedIn: '#' },
+    { name: 'Rahul Krishna K R', role: 'Model Implementation', imgSrc: './img/29.png', linkedIn: '#' },
+    { name: 'Ranjan Shruti Indresh', role: 'Model Implementation', imgSrc: './img/30.png', linkedIn: '#' },
+    { name: 'Ridhin Issac Abraham', role: 'Model Implementation', imgSrc: './img/31.png', linkedIn: '#' },
+    { name: 'Ritushree Dey', role: 'Model Implementation', imgSrc: './img/32.png', linkedIn: '#' },
+    { name: 'Samson Sabu', role: 'Model Implementation', imgSrc: './img/33.png', linkedIn: '#' },
+    { name: 'Sanjay S', role: 'Model Implementation', imgSrc: './img/34.png', linkedIn: '#' },
+    { name: 'Swapnali Singh', role: 'Model Implementation', imgSrc: './img/35.png', linkedIn: '#' },
+    { name: 'Swetha R', role: 'Model Implementation', imgSrc: './img/36.png', linkedIn: '#' },
+    { name: 'Victor Jose I J', role: 'Model Implementation', imgSrc: './img/37.png', linkedIn: '#' },
+    { name: 'Reeve R Mathew', role: 'Model Implementation', imgSrc: './img/38.png', linkedIn: '#' },
+
+
+    // Research Team
+    { name: 'Akshithaa Parvathavardhini', role: 'Research Team', imgSrc: './img/39.png', linkedIn: '#' },
+    { name: 'Manashwy Padhi', role: 'Research Team', imgSrc: './img/40.png', linkedIn: '#' },
+    { name: 'Vikash Krishna R', role: 'Research Team', imgSrc: './img/41.png', linkedIn: '#' },
+    { name: 'Vivegavane D', role: 'Research Team', imgSrc: './img/42.png', linkedIn: '#' },
+
   ];
 
   return (
@@ -65,7 +83,7 @@ const AboutUs = () => {
   <div className="image-container">
     <img
       src="./img/PA.jpg"
-      alt="Data Protection"
+      alt="Predictive Analysis"
       className="overview-image"
     />
   </div>
@@ -211,6 +229,26 @@ const AboutUs = () => {
             </div>
           ))}
         </div>
+
+                {/* Research Team */}
+                <h3>Research Team</h3>
+        <div className="team-grid">
+          {teamMembers.filter(member => member.role.includes('Research')).map((member, index) => (
+            <div className="team-member" key={index}>
+              <img
+                src={member.imgSrc}
+                alt={member.name}
+                className="member-image"
+              />
+              <h3>{member.name}</h3>
+              <p>{member.role}</p>
+              <a href={member.linkedIn} target="_blank" rel="noopener noreferrer">
+                <img src="./img/link.png" alt="LinkedIn" className="linkedin-icon" />
+              </a>
+            </div>
+          ))}
+        </div>
+
       </section>
     </div>
   );
